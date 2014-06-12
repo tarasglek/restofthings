cb3fd7dee45909107c7f14763a82c0ed77c85226
Taras Glek <github@glek.net>
2014-06-12 16:14:50 -0700

    got rid of python crap

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ slave - code that runs on pi
 ##Webiopi
 # Remove auth
 sudo rm /etc/webiopi/passwd
+
+
+### Flow
+* obtain device & plug it into network/power
+* go to user.restofthings.com to find device listed
+* Click link to go to dev ui(eg webiopi pinout page)
+* Assign tags to device
+* Have home automation software look up device by those tags
+** Alternative: have some software deployed on one of the devices and do ^
+* Enjoy
--- slave/server.js
+++ slave/server.js
@@ -1,3 +1,8 @@
+var net = require("net");
+var http = require("http");
+var fs = require("fs");
+var crypto = require("crypto");
+var url = require("url");
 var express = require('express');
 var getRawBody = require('raw-body');
 var fs = require('fs');
@@ -69,3 +74,83 @@ app.get("/features", function (req, res) {
 var server = app.listen(config.port, function() {
     console.log('Listening on port %d', server.address().port);
 });
+
+
+function getUUID() {
+  var dir = "/sys/class/net/";
+  var interfaces = fs.readdirSync(dir);
+  //interfaces.sort();
+  var macs = interfaces.map(function (interface) {
+    return fs.readFileSync(dir + interface + "/address");
+  }).join("");
+  var sha1 = crypto.createHash('sha1');
+  sha1.update(macs);
+  return sha1.digest('hex');
+}
+
+function getOwnAddress(port, hostname, callback) {
+  var socket = net.createConnection(port, hostname);
+  socket.on('connect', function () {
+    var self = "http://" + socket.address().address + ":" + config.port;
+    socket.destroy();
+    callback(null, self)
+  });
+  socket.on('error', function (err) {
+    socket.destroy();
+    callback(err);
+  });
+}
+
+function registerWithDirectoryServer() {
+  var retryTimeout = 5000;
+  var directoryUrl = url.parse(config.directoryServer);
+
+  function errorHandler(err) {
+    console.error("Failed to connect to directory server:" + config.directoryServer, err.message);
+    console.error("Will retry in " + retryTimeout/1000 + " seconds"); 
+    setTimeout(registerWithDirectoryServer, retryTimeout);
+  }
+
+  getOwnAddress(directoryUrl.port, directoryUrl.hostname, function (err, url) {
+    if (err) {
+      return errorHandler(err);
+    }
+    
+    var uuid = getUUID();
+    var payload = JSON.stringify({"uuid":uuid, "localURL":url});
+    var options = {
+      host: directoryUrl.hostname,
+      port: directoryUrl.port,
+      path: '/thing/' + uuid,
+      method: 'PUT',
+      headers: {"Content-Length":payload.length}
+    };
+
+    var req = http.request(options, function(res) {
+      //console.log('STATUS: ' + res.statusCode);
+      //console.log('HEADERS: ' + JSON.stringify(res.headers));
+      //res.setEncoding('utf8');
+      var ret = "";
+      res.on('data', function (chunk) {
+        ret += chunk;
+      });
+      res.on('end', function () {
+        if (res.statusCode == 200) {
+          console.log("Registered " + payload);
+        } else {
+          console.error("Directory server rejected with code "+res.statusCode+": " + ret);
+        }
+      });
+    });
+
+    req.on('error', function(e) {
+      errorHandler(e);
+    });
+
+    // write data to request body
+    req.write(payload);
+    req.end();
+  });
+}
+
+registerWithDirectoryServer();
